25d4d5936f0ab38601aacfd87b10d5a9a987cd7f
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-04 03:38:52 +0300

    first test added

---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    @Test
+    void shouldChangeCurrentRadioStation() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioStation());
+        radio.setCurrentRadioStation(6);
+        assertEquals(7, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void shouldCalculateMaxRadioStation() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioStation());
+        radio.setCurrentRadioStation(9);
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void shouldCalculateMinRadioStation() {
+        Radio radio = new Radio();
+        assertEquals(0, radio.getCurrentRadioStation());
+        radio.setCurrentRadioStation(0);
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
